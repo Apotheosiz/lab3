e7a255e87ef17b94ffbdb1eaec780923f8d086d8
Apotheosiz <sascha.kasapovski@student.kyh.se>
2021-06-21 08:15:15 +0200

    testing mocha

---
+++ app.js
@@ -0,0 +1,45 @@
+// import
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// listen on port
+app.listen(port, () => console.info('Listening on port ${port}'));
+
+var addAmmount = 0;
+
+// Create a function that displays random number
+function randomNumber(max) {
+    let randObj = {
+        "number": Math.floor(Math.random() * max)
+    };
+    return randObj
+};
+
+// Function that counts the amount of vowels in a word. Source: https://www.programiz.com/javascript/examples/count-vowels
+function countVowel(countallthevowels) {
+    let countVowel = {
+        "word": countallthevowels,
+        "vowels": count = countallthevowels.match(/[aeiouåäö]/gi).length
+    };
+    return countVowel
+};
+
+
+// Endpoints
+app.get('/api/random', (req, res) => {
+    return res.send(randomNumber(1024));
+});
+
+app.get('/api/count_vowels/:word', (req, res) => {
+    return res.send(countVowel(req.params.word));
+});
+
+app.get('/api/custom_random/:num', (req, res) => {
+    return res.send(randomNumber(req.params.num));
+});
+
+app.get('/api/counter/add', (req, res) => {
+    addAmount++;
+    res.send({ message: 'added 1 to counter' });
+});
---
+++ test.js
@@ -0,0 +1,48 @@
+const expect = require('chai').expect;
+const request = require('request');
+
+
+describe('Random', () => {
+    it('Random number between 0 and 1023', (done) => {
+        request.get('http://localhost:3000/api/random', (err, res, body) => {
+            expect(JSON.parse(body).number).to.be.within(0, 1023);
+            expect('Content-Type', /json/);
+            expect(200);
+            done();
+        });
+    });
+});
+
+describe('Vowels', () => {
+    it('Counts the amount of vowels in word', (done) => {
+        var word = 'aeiouyåäö'
+        request.post('http://localhost:3000/api/counter/vowels:word' + word, (err, res, body) => {
+            expect(JSON.parse(body).amountOfVowels).to.equal(9);
+            expect('Content-Type', /json/);
+            expect(200);
+            done();
+        });
+    });
+});
+
+describe('Number', () => {
+    it('Calls a random number', (done) => {
+        request.get('http://localhost:3000/api/counter/:num', (err, res, body) => {
+            expect(JSON.parse(body).currentAmount).to.be.a('number');
+            expect('Content-Type', /json/);
+            expect(200);
+            done();
+        });
+    });
+});
+
+describe('Add', () => {
+    it('Added 1 to counter', (done) => {
+        request.get('http://localhost:3000/api/counter/add', (err, res, body) => {
+            expect(JSON.parse(body).message).to.equal('added 1 to counter');
+            expect('Content-Type', /json/);
+            expect(200);
+            done();
+        });
+    });
+});
